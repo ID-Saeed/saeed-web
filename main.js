136e426997c5d23a4e2ae3a25d74c604a9212323
ID-Saeed <112485076+ID-Saeed@users.noreply.github.com>
2022-10-22 15:59:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Select The Elements
+let theElementsUl = document.querySelector(".links ul");
+let theMenu = document.querySelector(".links").addEventListener("click", function () {
+    theElementsUl.classList.toggle("show");
+});
